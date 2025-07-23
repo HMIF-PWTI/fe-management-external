67a674753341654a0591430559fe6a798c7fadda
Rifqi1012 <rifqimh1210@gmail.com>
2025-07-23 21:24:17 +0700

    [UPDATE] : Coming Soon Page On Blog Page

--- src/pages/InfoKp/index.tsx
+++ src/pages/InfoKp/index.tsx
@@ -31,6 +31,48 @@ const InfoKp = () => {
       <h1 className="text-3xl lg:text-5xl text-center font-semibold italic text-primary2">
         INFO PERUSAHAAN KERJA PRAKTEK
       </h1>
+      <div className="w-full self-start space-y-3">
+        <h2 className="text-xl font-medium text-primary2 sm:text-2xl">
+          Tahapan Prosedur Kerja Praktek
+        </h2>
+        <ul className="list-disc list-inside space-y-2 text-gray-800">
+          <li>
+            Mengunjungi Perusahaan yang akan dilamar dan menanyakan persyaratan
+            untuk kerja praktek
+          </li>
+          <li>Program Studi hanya menyediakan surat pengantar kerja praktek</li>
+          <li>
+            Untuk persyaratan selain surat pengantar kerja praktek seperti CV,
+            Transkrip Nilai dll. dipersiapkan oleh mahasiswa secara mandiri
+          </li>
+          <li>
+            Untuk Transkrip Nilai bisa di akses di website dashboard unikom yang
+            bisa di akses pada link berikut:{" "}
+            <a
+              href="https://mahasiswa.unikom.ac.id/"
+              target="_blank"
+              className="italic underline text-blue-500 hover:text-blue-700"
+            >
+              Tekan Disini
+            </a>
+          </li>
+          <li>
+            Setelah Persyaratan terpenuhi, mahasiswa mengirimkan persyaratan ke
+            perusahaan
+          </li>
+          <li>
+            Setelah menerima surat balasan dari perusahaan, mahasiswa harus
+            mengkonfirmasi ke email:{" "}
+            <a
+              href="https://mail.google.com/mail/u/0/?view=cm&tf=1&fs=1&to=roby.irawan@email.unikom.ac.id"
+              target="_blank"
+              className="font-medium italic underline text-blue-500 hover:text-blue-700"
+            >
+              roby.irawan@email.unikom.ac.id
+            </a>
+          </li>
+        </ul>
+      </div>
       <div className="w-full overflow-hidden rounded-xl border border-primary2">
         <table className="w-full border-collapse">
           <thead className="hidden lg:table-header-group bg-primary2 text-white">
